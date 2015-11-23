1ece67e3b9311580716e39189d4fd2162565271c
bailpaqu <bailpaqu420@ed.amdsb.ca>
2015-11-23 13:31:37 -0500

    Yo

--- stars/Form1.cs
+++ stars/Form1.cs
@@ -16,5 +16,40 @@ namespace stars
         {
             InitializeComponent();
         }
+
+       
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            StarField(100);
+        }
+
+        private void StarField(int y)
+        {
+            Graphics formGraphics = this.CreateGraphics();
+            SolidBrush drawBrush = new SolidBrush(Color.Black);
+
+            int x = 0;
+            int xcoord, ycoord, size, rcolor, gcolor, bcolor;
+            Random ranNum = new Random();
+
+            while (x <= y)
+            {
+                rcolor = ranNum.Next(1, 255);
+                gcolor = ranNum.Next(1, 255);
+                bcolor = ranNum.Next(1, 255);
+
+                drawBrush.Color = Color.FromArgb(rcolor, bcolor, gcolor);
+
+                xcoord = ranNum.Next(1, 300);
+                ycoord = ranNum.Next(1, 300);
+                size = ranNum.Next(1, 10);
+
+                formGraphics.FillRectangle(drawBrush, xcoord, ycoord, size, size);
+
+                x++;
+            }
+        }
     }
 }
